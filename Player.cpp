bf2873cd995546b0c8dbd4b2702a1ada1d1e0f94
petr <petr@debian-2017>
2017-05-08 18:59:53 +0200

    Added makeshift Player class.

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+#include "Player.h"
+
+Player::Player() : Entity(0, 'P', 500, 1, 10, 1) {
+}
+
---
+++ Player.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Player.h
+ * Author: petr
+ *
+ * Created on May 8, 2017, 6:47 PM
+ */
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "Entity.h"
+
+class Player : public Entity {
+public:
+	Player();
+private:
+
+};
+
+#endif /* PLAYER_H */
+
